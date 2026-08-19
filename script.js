07bdc0e3ec95b9a4a288de4018a88693191ab2a8
krishna <krishnapatil1042005@gmail.com>
2026-08-19 23:25:45 +0530

    add enter key support, refactor shared loading function

--- script.js
+++ script.js
@@ -3,28 +3,38 @@ const button = document.querySelector("button");
 const result = document.querySelector("#result");
 let username = "";
 
-//click listener on the button
-
-button.addEventListener("click", function () {
-  result.innerHTML = `<p>loading...</p>`
-  async function fetchUserData(username) {
-    const response = await fetch(`https://api.github.com/users/${username}`);
-    const data = await response.json();
-    if (data.message === "Not Found") {
-      result.innerHTML = `<p>User not found</p>`;
-      console.log("user not found");
-    } else {
-      console.log(data);
-      result.innerHTML = `
+const fetchData = async function fetchUserData(username) {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  if (data.message === "Not Found") {
+    result.innerHTML = `<p>User not found</p>`;
+    console.log("user not found");
+  } else {
+    console.log(data);
+    result.innerHTML = `
     <img src="${data.avatar_url}">
     <h2>${data.login}</h2>;
     <p>${data.bio}</p>;
     <p>Repos: ${data.public_repos}</p>
     <P>followers: ${data.followers}</P>
     <P>following: ${data.following}</P>`;
-    }
   }
-  fetchUserData(input.value);
+};
+
+//click listener on the button
+
+const loading = function () {
+  result.innerHTML = `<p>loading...</p>`;
+  fetchData(input.value);
+};
+
+button.addEventListener("click", function () {
+  loading();
 });
 
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    loading();
+  }
+});
 
